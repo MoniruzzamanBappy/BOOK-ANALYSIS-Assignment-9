9edf60ca6276d6fa12c4fa68e545e05b1d1efd6e
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-04-03 22:51:21 +0600

    error page design

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./Error.css";
 
 const Error = () => {
-    return (
-        <div>
-            <h1>this is 404</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold">404</h1>
+      <h3 className="font-medium text-4xl">page not found</h3>
+      <p className="font-medium text-4xl">Boo, looks like a ghost stole this page!</p>
+    </div>
+  );
 };
 
 export default Error;
