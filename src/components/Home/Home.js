cc68069017ce1f549cfbdea5fbfedf6f95afe61d
MoniruzzamanBappy <mdmoniruzzamanbappy@gmail.com>
2022-04-04 10:45:30 +0600

    home page alignment edit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,18 @@
 import "./Home.css";
-import Review from './../Review/Review';
+import Review from "./../Review/Review";
 import { Link } from "react-router-dom";
-import useReviews from './../hooks/useReviews';
+import useReviews from "./../hooks/useReviews";
 
 const Home = () => {
-    const [reviews, setReviews] = useReviews()
-   
+  const [reviews, setReviews] = useReviews();
+
   return (
     <div className="px-16">
       <div className="book-cover">
-        <div>
-          <h1 className="text-4xl font-extrabold mb-8 mt-4">A Game of Thrones</h1>
+        <div className="my-auto">
+          <h1 className="text-4xl font-extrabold mb-8 mt-4">
+            A Game of Thrones
+          </h1>
           <p>
             Here is the first volume in George R. R. Martin's magnificent cycle
             of novels that includes A Clash of Kings and A Storm of Swords. As a
@@ -22,16 +24,19 @@ const Home = () => {
             destined to stand as one of the great achievements of imaginative
             fiction.
           </p>
+          <button class="bg-slate-400 px-12 py-3 mt-4 rounded-full ...">
+            <Link to="/aboutus">Learn More...</Link>
+          </button>
         </div>
         <img src="cover-pic.jpg" alt="" />
       </div>
       <h1 className="mb-4 font-bold text-4xl">COMMUNITY REVIEWS</h1>
-        <hr />
-      {
-          reviews.slice(0,3).map(review=><Review key={review.id} review={review}></Review>)
-      }
+      <hr />
+      {reviews.slice(0, 3).map((review) => (
+        <Review key={review.id} review={review}></Review>
+      ))}
       <button class="bg-slate-400 px-12 p-3 mb-4 rounded-full ...">
-          <Link to="/reviews">See All...</Link>
+        <Link to="/reviews">See All...</Link>
       </button>
     </div>
   );
